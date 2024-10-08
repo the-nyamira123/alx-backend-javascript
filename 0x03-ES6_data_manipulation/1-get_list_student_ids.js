f014e28bdec893bbcd6fddfd035d1b410b4b71c7
root <nathanslay7@gmail.com>
2024-10-08 16:31:47 +0000

    Create a function getListStudentIds that returns an array of ids from a list of object.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,16 +1,8 @@
-/**
- * Retrieves ids from a list of students.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
- * @author <https://github.com/the-nyamira123/alx-backend-javascript> 
- * @returns
- */
-export default function getListStudentIds(students) {
-  if (students instanceof Array) {
-    return students.map((student) => student.id);
+const getListStudentIds = (myArray) => {
+  if (!Array.isArray(myArray)) {
+    return [];
   }
-  return [];
-}
+  return myArray.map((studentId) => studentId.id);
+};
+
+export default getListStudentIds;
