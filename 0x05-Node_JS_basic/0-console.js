c71979c5a34f7c66d4f45509334ca0a1d5ed1765
the-nyamira123 <nathanslay7@gmail.com>
2024-11-26 13:21:26 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Nyamira Nathan Orangi  <https://github.com/the-nyamira123>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
